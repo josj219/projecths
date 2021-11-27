f5851bbb0f1916d8908693918e388dcb8500f9de
unknown <josj219@gmail.com>
2021-11-27 14:51:24 +0900

    1127152

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,8 @@
 import "../css/style.css";
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
