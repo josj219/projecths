e0e95e324f6fadb3084eaa73c19d3f26402b1e9d
unknown <1900629@device.glovis.net>
2021-12-03 16:31:46 +0900

    1204

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -101,6 +101,16 @@ const handleMouseLeave = () => {
   controlsTimeout = setTimeout(hideControls, 3000);
 };
 
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
