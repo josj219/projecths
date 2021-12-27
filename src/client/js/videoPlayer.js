b490ba260a0c4cd202c5416f2dfc7084d3bd238d
unknown <1900629@device.glovis.net>
2021-12-27 19:30:54 +0900

    1227

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -17,9 +17,12 @@ let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
+console.log(video);
+
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
+    console.log("CLICKED");
   } else {
     video.pause();
   }
